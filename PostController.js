177ebd206d031b135d8f9a92229f0aa455b53fd8
iFrodo <igor@azinet.ru>
2023-11-22 12:14:44 +0300

    post-service

--- PostController.js
+++ PostController.js
@@ -1,4 +1,6 @@
 import Post from "./Post.js";
+import PostService from "./PostService.js";
+
 
 
 //это круд класс
@@ -6,8 +8,7 @@ class PostController {
 
     async create(req, res) {
         try {
-            const { author, title, content, picture } = req.body;
-            const post = await Post.create({ author, title, content, picture })
+            const post = await PostService.create(req.body)
             res.status(200).json(post)
         } catch (error) {
             res.status(500).json(e)
@@ -16,7 +17,7 @@ class PostController {
 
     async getAll(req, res) {
         try {
-            const posts = await Post.find();
+            const posts = await PostService.getAll(req.body);
             return res.json(posts)
 
         } catch (error) {
@@ -27,10 +28,7 @@ class PostController {
     async getOne(req, res) {
         try {
             const { id } = req.params
-            if (!id) {
-                res.status(400).json({ message: 'не указан id' })
-            }
-            const post = await Post.findById(id)
+            const post = await PostService.getOne(id)
             return res.json(post)
         } catch (error) {
             res.status(500).json(error)
@@ -43,7 +41,7 @@ class PostController {
             if (!post._id) {
                 res.status(400).json({ message: 'не указан id' })
             }
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, { new: true });
+            const updatedPost = await PostService.update(post._id, post, { new: true });
             return res.json(updatedPost)
         } catch (error) {
             res.status(500).json(error)
@@ -56,7 +54,7 @@ class PostController {
             if (!id) {
                 res.status(400).json({ message: 'не указан id' })
             }
-            const post = await Post.findByIdAndDelete(id)
+            const post = await PostService.delete(id)
             return res.json(post)
         } catch (error) {
             res.status(500).json(error)
